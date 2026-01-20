5d0b3f1b254861dda4b0eb6a609d40dd42e16ea0
ayakadd98 <139221990+ayakadd98@users.noreply.github.com>
2026-01-20 21:13:32 +0100

    Create ex5.c

---
+++ conditions/ex5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+  int a;
+  printf("please enter the number:\n");
+  scanf(" %d",&a);
+if(a%2 ==0)
+  printf("paire");
+  else
+  printf("impaire");
+    return 0;
+}
