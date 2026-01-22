810619c96a0fa533c954acec9685ea57bd956c0a
ayakadd98 <139221990+ayakadd98@users.noreply.github.com>
2026-01-22 20:48:31 +0100

    Create ex3.c

---
+++ loops/ex3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+  int n,i,s;
+  printf("this program Calculate the Sum of Up to 8 Numbers, Stopping When a Negative Number Is Entered.\n");
+ s=0;
+ for(i=0;i<8;i++){
+   printf("enter n :\n");
+   scanf("%d",&n);
+   if(n<0) break;
+   s=s+n;
+   }
+  printf("%d",s);
+    return 0;
+}
